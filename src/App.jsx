fdc046edb74431f62eeac55c6a57b19c75def8b1
Olufemi Benjamin <benjaminolufemi16@gmail.com>
2024-01-23 04:15:01 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from './context/ThemeContext'
-import Register from './pages/userdomain/Register'
-import Login from './pages/userdomain/Login'
-import Wallet from './pages/userdomain/Wallet'
-import Home from './pages/Home'
-import Dashboard from './pages/app/Dashboard'
-import Announcement from './pages/app/Announcement'
-import Teams from './pages/app/Teams'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import Register from "./pages/userdomain/Register";
+import Login from "./pages/userdomain/Login";
+import Wallet from "./pages/userdomain/Wallet";
+import Home from "./pages/Home";
+import Dashboard from "./pages/app/Dashboard";
+import Announcement from "./pages/app/Announcement";
+import Teams from "./pages/app/Teams";
 
 function App() {
   return (
@@ -18,12 +18,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/announcement" element={<Announcement />} />
-          <Route path='/teams' element={<Teams />} />
-          <Route path='/wallet' element={<Wallet />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/wallet" element={<Wallet />} />
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
